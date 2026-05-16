e6f794601b7f26db0c5461dba89c0b34a212f3a5
BluiWulf <bluiwulf@gmail.com>
2026-05-16 14:21:57 -0400

    Testing commit issue

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"github.com/bluiwulf/pokedex/internal/pokeapi"
 	"time"
+
+	"github.com/bluiwulf/pokedex/internal/pokeapi"
 )
 
 func main() {
-	apiClient := pokeapi.NewClient(5 * time.Second, 5 * time.Minute)
+	apiClient := pokeapi.NewClient(5*time.Second, 5*time.Minute)
 	cfg := &apiConfig{
 		apiClient: apiClient,
 	}
 	startPokedex(cfg)
 }
-
